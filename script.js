d5575a6d6b0e98bc7fb564cad8d6ae9c3699f72c
afardosa888-art <afardosa888@gmail.com>
2026-07-16 22:55:30 +0300

    i insert logic operators in .js

--- script.js
+++ script.js
@@ -2,15 +2,17 @@
 console.log("page uploading ...")
 const display = document.querySelector(".display")
 const numbers = document.querySelectorAll(".numbers")
-const operators = document.querySelectorAll(".operators")
+const operators = document.querySelectorAll(".operator")
 const clearbtn = document.querySelectorAll("#clear")
 const deletebtn = document.querySelectorAll("#delete")
+const equalbtn = document.querySelectorAll("#equal")
 
 // console.log(display, "display clicked")
 // console.log(numbers, "num clicked")
 // console.log(operators, "op clicked")
 // console.log(clear, "clear clicked")
 // console.log(deletebtn, "delete clicked")
+// console.log(equalbtn, "equal clicked")
 
 let currentInput = "";
 let intialInput = "0";
@@ -30,10 +32,11 @@ numbers.forEach(btn => {
 })
 operators.forEach(btn => {
    btn.addEventListener("click", () => {
+      // if(btn.textContent === "=") return;
       currentInput += btn.textContent;
       display.value = currentInput;
-   })
-})
+   });
+});
 
 // display intialization
 
