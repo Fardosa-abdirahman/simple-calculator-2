e1f41e07e7362310bb2b70709a226151bb7ac8f7
afardosa888-art <afardosa888@gmail.com>
2026-07-16 19:07:41 +0300

    i add display function in script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-const display = document.querySelectorAll(".display")
+
+console.log("page uploading ...")
+const display = document.querySelector(".display")
 const numbers = document.querySelectorAll(".numbers")
 const operators = document.querySelectorAll(".operators")
 const clearbtn = document.querySelectorAll("#clear")
 const deletebtn = document.querySelectorAll("#delete")
+
+// console.log(display, "display clicked")
+// console.log(numbers, "num clicked")
+// console.log(operators, "op clicked")
+// console.log(clear, "clear clicked")
+// console.log(deletebtn, "delete clicked")
+
+let currentInput = "";
+let intialInput = "0";
+
+function updateDisplay() {
+
+   (display.value = intialInput )
+}
+
+console.log(updateDisplay());
+function updateNumbers() {
+   
+}
+
+// display intialization
+
